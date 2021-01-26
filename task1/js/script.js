9b9230ec8b00bbed637642b650f3d43eddb941ee
pavelkolechits <75511210+pavelkolechits@users.noreply.github.com>
2021-01-26 05:59:54 +0300

    task1

---
+++ task1/js/script.js
@@ -0,0 +1,33 @@
+//======    1    ======
+// let a = +prompt("Введите число");
+// alert(a**2);
+
+
+// ======   2   ======
+// let numb1 = +prompt("Введите число");
+// let numb2 = +prompt("Введите число");
+// let m = (numb1 + numb2)/2;
+// alert("Среднее арифметическое " + m);
+
+
+// ======   3   ======
+// let area = +prompt("Введите сторону квадрата");
+// alert("Площадь квадрата " + area**2);
+
+// ======   4   ======
+// const mile = 0.621371;
+// let klm = +prompt("Если Вы введете расстояние в километрах, получите расстояние в милях");
+// let summDistance = mile*klm;
+// alert("Расстояние в милях " +  summDistance);
+
+
+// ======   5   ======
+let arg1 = +prompt("Введите число");
+let arg2 = +prompt("Введите число");
+let summ, mns, pltp, dvsn; 
+summ = arg1 + arg2;
+mns = arg1 - arg2;
+pltp = arg1 * arg2;
+dvsn = arg1 / arg2;
+alert("Сумма=" + summ + "\n" +"Вычетание=" + mns + "\n" + "Умножение=" + pltp + "\n" + "Деление=" + dvsn);
+
